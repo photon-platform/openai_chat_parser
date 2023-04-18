51a10adf40cab43fc3443d8188dbe5db14ae180f
phiarchitect <phi@phiarchitect.com>
2023-04-17 20:04:16 -0700

    add docstrings

--- count_words.py
+++ count_words.py
@@ -1,6 +1,19 @@
+"""
+word_count_utility
+
+A utility script to calculate the word count in '000-full_transcript.md' files
+located within a directory and its subdirectories, and print the total word count.
+"""
+
 import os
 
 def count_words_in_file(file_path):
+    """
+    Count the words in a given file.
+
+    :param file_path: The path to the file.
+    :return: The word count in the file.
+    """
     with open(file_path, 'r') as f:
         text = f.read()
         words = text.split()
@@ -8,6 +21,13 @@ def count_words_in_file(file_path):
     return word_count
 
 def find_transcripts_and_count_words(start_directory):
+    """
+    Find '000-full_transcript.md' files in a directory and its subdirectories
+    and calculate the total word count.
+
+    :param start_directory: The starting directory to search for '000-full_transcript.md' files.
+    :return: A tuple containing a dictionary with individual file word counts and the total word count.
+    """
     word_counts = {}
     total_word_count = 0
     
